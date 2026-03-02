646576f84bdd81d5c0c9cd3ccb8c52e4b6a2d3e0
yaser <yaser@example.com>
2026-03-02 14:36:04 +0300

    auto deploy

--- deploy.ps1
+++ deploy.ps1
@@ -8,6 +8,6 @@ Write-Host "==> Git Push"
 git push origin main
 
 Write-Host "==> SSH Deploy"
-ssh mahsum@168.231.125.93 "/home/mahsum/instaapp/deploy.sh"
+ssh -i C:\Users\yaser\.ssh\id_rsa_instaapp -o IdentitiesOnly=yes mahsum@168.231.125.93 "/home/mahsum/instaapp/deploy.sh"
 
 Write-Host "==> Deploy Finished"
